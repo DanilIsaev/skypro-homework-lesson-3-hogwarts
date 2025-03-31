d0a3d453f69283bfa1d3521810236a6214017e40
danie <daniel.isaes@mail.ru>
2025-03-31 20:57:53 +0300

    Добавлены проверки файла и директорий

--- src/main/java/ru/hogwarts/school/service/AvatarServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/AvatarServiceImpl.java
@@ -3,8 +3,19 @@ package ru.hogwarts.school.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
+import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.AvatarRepository;
 import ru.hogwarts.school.serviceInterface.AvatarService;
+import ru.hogwarts.school.serviceInterface.StudentService;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+import static org.apache.commons.lang3.StringUtils.substring;
 
 @Service
 @Transactional
@@ -14,6 +25,24 @@ public class AvatarServiceImpl implements AvatarService {
 
     private AvatarService avatarService;
     private AvatarRepository avatarRepository;
+    private final StudentService studentService;
+
+    public AvatarServiceImpl(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @Override
+    public Avatar uploadAvatar(Long id, MultipartFile file) throws IOException {
+        Student student = studentService.findStudent(id);
+
+        Path filePath = Path.of(coversDir, id + "." + getException(Objects.requireNonNull(file.getOriginalFilename())));
+        Files.createDirectories(filePath.getParent()); // Проверяет есть ли необходимые директории для файла
+        Files.deleteIfExists(filePath); // Проверяет наличие на файла и в случае, если файл существует удаляет его предыдущий экземпляр
 
 
+    }
+
+    private String getException(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
 }
--- src/main/java/ru/hogwarts/school/serviceInterface/AvatarService.java
+++ src/main/java/ru/hogwarts/school/serviceInterface/AvatarService.java
@@ -1,4 +1,10 @@
 package ru.hogwarts.school.serviceInterface;
 
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
+
+import java.io.IOException;
+
 public interface AvatarService {
+    Avatar uploadAvatar(Long id,MultipartFile file) throws IOException;
 }
