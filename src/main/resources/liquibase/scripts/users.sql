31cb3bcf26e7882332711f949428e15d420b7d5b
danie <daniel.isaes@mail.ru>
2025-04-01 20:39:23 +0300

    В файл миграции добавлены запросы для индексации

--- src/main/resources/liquibase/scripts/users.sql
+++ src/main/resources/liquibase/scripts/users.sql
@@ -13,3 +13,8 @@ ALTER TABLE users ADD name TEXT;
 DROP TABLE users;
 
 -- changeset disaev:1
+CREATE INDEX index_for_searching_by_student_name ON student(name)
+
+-- changeset disaev:3
+CREATE INDEX  index_for_searching_by_faculty_name ON faculty (name);
+CREATE INDEX  index_for_searching_by_faculty_color ON faculty (color);
