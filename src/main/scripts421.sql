bf435b785e727553e95f86182eb7fdbbc5b0031b
danie <daniel.isaes@mail.ru>
2025-04-01 16:35:15 +0300

    Выполнено 1 задание и добавлено в БД

--- src/main/scripts421.sql
+++ src/main/scripts421.sql
@@ -1,3 +1,25 @@
+/*UPDATE student
+SET age = 20
+WHERE age < 16 OR age IS NULL;*/
+
+-- 1. Возраст не может быть меньше 16 лет
+ALTER TABLE public.student
+    ADD CONSTRAINT chk_student_age CHECK (age >= 16);
+
+-- 2. Имена студентов должны быть уникальными и не null
+ALTER TABLE public.student
+    ALTER COLUMN name SET NOT NULL;
+
+ALTER TABLE public.student
+    ADD CONSTRAINT uniq_student_name UNIQUE (name);
+
+-- 3. Установка возраста по умолчанию 20 лет, если не указан
+ALTER TABLE public.student
+    ALTER COLUMN age SET DEFAULT 20;
+
+-- 4. Пара "название факультета" - "цвет факультета" должна быть уникальной
+ALTER TABLE faculty
+    ADD CONSTRAINT uniq_faculty_name_color UNIQUE (name, color);
 
 
 -- Сначала создаем таблицу cars, так как people будет ссылаться на нее
