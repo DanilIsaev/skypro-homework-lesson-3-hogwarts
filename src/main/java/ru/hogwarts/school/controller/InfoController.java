90a59bdf12592162bf6aadcd5fe8c59a40c84f3b
danie <daniel.isaes@mail.ru>
2025-04-02 13:23:47 +0300

    Логирование: добавлен эндпоинт получения порта для задания 2

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("port")
 public class InfoController {
 
+    @Autowired
+    Environment environment;
+
     @GetMapping
-    public String info() {}
+    public String info() {
+        return environment.getProperty("local.server.port");
+    }
 }
