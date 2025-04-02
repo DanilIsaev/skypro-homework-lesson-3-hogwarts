a7248b7c7648266f56af20489261ca48c803ab08
danie <daniel.isaes@mail.ru>
2025-04-02 13:15:40 +0300

    Логирование: создан контроллер InfoController для задания 2

---
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("port")
+public class InfoController {
+
+    @GetMapping
+    public String info() {}
+}
